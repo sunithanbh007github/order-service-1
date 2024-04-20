7d3438be8b493b18d06e3faf1374335b50812768
nvasireddygithub <naveen.hbs007@gmail.com>
2024-04-20 11:37:41 +0530

    first commit

---
+++ src/test/java/com/polarbookshop/OrderService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
