e571884390a22653a22446145afb0dc1f3a3b011
nvasireddygithub <naveen.hbs007@gmail.com>
2024-07-20 21:02:24 +0530

    added kustomize

--- src/main/java/com/polarbookshop/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
